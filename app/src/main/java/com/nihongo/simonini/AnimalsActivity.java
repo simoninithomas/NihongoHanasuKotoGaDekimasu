22fc232373721c6ccc5916927f47d52c9167aed4
Simonini <simonini_thomas@outlook.fr>
2017-01-17 07:46:20 +0100

    - Add return <- arrow. - Finish the first version of the app

--- app/src/main/java/com/nihongo/simonini/AnimalsActivity.java
+++ app/src/main/java/com/nihongo/simonini/AnimalsActivity.java
@@ -3,8 +3,10 @@ package com.nihongo.simonini;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -95,11 +97,23 @@ public class AnimalsActivity extends AppCompatActivity {
         }
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_view);
+
+        // Add up action
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // Initialise AudioManager by calling getSystemService
         // and passing in the audio service constant
